6ffedafb27064136b5779d3381ad85b1ae0f1912
karlsweet <obykarlsweet@gmail.com>
2023-06-23 05:13:31 -0700

    cvariables

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+       	int d;
+
+	for (d = '0'; d <= '9'; d++)
+	putchar(d);
+	putchar('\n'); 
+
+	return (0);
+}
