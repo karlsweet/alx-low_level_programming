cb773da684eba3ad307b23ca164cd494c894171f
karlsweet <obykarlsweet@gmail.com>
2023-06-23 05:40:59 -0700

    cvariables

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Prints if number is positive, zero or negative
+ *
+ * Return: always (Success)
+ *
+ */
+int main(void)
+{
+	int d;
+	char low;
+
+	for (d = '0'; d <= 'z'; d++)
+	putchar(d);
+
+	for (low = 'a'; low <= 'f'; low++)
+	putchar(low);
+	putchar('\n');
+	return (0);
+}
